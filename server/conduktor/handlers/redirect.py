0fb4f002cb954732003137cb163c913e7e605995
Marco Tabini <mt@noom.com>
2018-08-18 08:51:54 -0400

    - Properly handle query parameters

--- server/conduktor/handlers/redirect.py
+++ server/conduktor/handlers/redirect.py
@@ -1,8 +1,12 @@
+import urllib.parse as urlparse
+
 from sqlalchemy.orm.exc import NoResultFound
+from urllib.parse import urlencode
 
 from conduktor.handlers.base import BaseHandler
 from conduktor.models import URL
 
+import logging
 
 class RedirectHandler(BaseHandler):
     def get(self, slug):
@@ -10,7 +14,15 @@ class RedirectHandler(BaseHandler):
             url = self.db.query(URL).filter(URL.slug==slug.lower(),URL.active==True).one()
             url.record_visit(self.db)
 
-            self.redirect(url.redirect)
+            original_url = list(urlparse.urlparse(self.request.full_url()))
+            redirect_url = list(urlparse.urlparse(url.redirect))
+
+            query = dict(urlparse.parse_qsl(redirect_url[4]))
+            query.update(dict(urlparse.parse_qsl(original_url[4])))
+
+            redirect_url[4] = urlencode(query)
+
+            self.redirect(urlparse.urlunparse(redirect_url))
         except NoResultFound:
             self.set_status(404, reason='Not Found')
             self.write('Not Found')
